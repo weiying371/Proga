e325426768761041d3bb66d21b4a3aec76a36f44
Stepovana Anna <aniastepovana@gmail.com>
2026-06-05 20:05:54 +0300

    homework.3.3.1

---
+++ GitHub/3.3.1/figure.py
@@ -0,0 +1,14 @@
+class Figure:
+    def dimension(self): return None
+
+    def perimetr(self): return None
+
+    def square(self): return None
+
+    def squareSurface(self): return None
+
+    def squareBase(self): return None
+
+    def height(self): return None
+
+    def volume(self): return 0
